81b066cbbde3a6722cb3f25f82a95f692f550423
Connor Imes <cimes@isi.edu>
2022-05-10 15:50:06 -0400

    quantization: integrate with quant_bits buffers configured on modules

--- edgepipe/quantization/hook.py
+++ edgepipe/quantization/hook.py
@@ -4,11 +4,10 @@ from .basic_op import tensor_encode, tensor_decode
 def forward_hook_quant_encode(module, input_tensor, output):
     """encode tensor in the forward hook (after each module)"""
     assert isinstance(output, tuple)
-    quant_bits = (8, 8)
+    quant_bits = module.quant_bits.tolist()
     comm_tuple = []
     for tensor in output:
         comm_tuple += tensor_encode(tensor, quant_bits[1])
-
     return tuple(comm_tuple)
 
 
@@ -20,10 +19,9 @@ def forward_pre_hook_quant_decode(module, input_tensor):
     # input_shape: to restore the original shape of the tensor needed for forward
     # scale_facto: the max value of the original uncomperssed tensor
     # shift: the min value of the original uncompressed tensor
-
     assert isinstance(input_tensor[0], tuple)
     input_tensor = input_tensor[0]
-    quant_bits = (8, 8)
+    quant_bits = module.quant_bits.tolist()
     forward_tensor = []
     assert len(input_tensor) % 4 == 0
     for i in range(len(input_tensor) // 4):
