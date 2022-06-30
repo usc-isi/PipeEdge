6c2f36973800136de9e3964e62d94bcc77ab8cee
Connor Imes <cimes@isi.edu>
2022-06-30 16:12:42 -0400

    model_cfg: add Python type hints

--- model_cfg.py
+++ model_cfg.py
@@ -1,6 +1,8 @@
 """Model configurations and default parameters."""
+from typing import Any, Callable, List, Tuple
 from torch.distributed import rpc as trpc
 from pipeedge.comm import rpc
+from pipeedge.models import ModuleShard
 from pipeedge.models.transformers.bert import BertTransformerShard
 from pipeedge.models.transformers.deit import DeiTTransformerShard
 from pipeedge.models.transformers.vit import ViTTransformerShard
@@ -34,23 +36,24 @@ _model_cfg_add('facebook/deit-small-distilled-patch16-224', 48, 'DeiT_S_distille
 _model_cfg_add('facebook/deit-tiny-distilled-patch16-224', 48, 'DeiT_T_distilled.npz',
                DeiTTransformerShard)
 
-def get_model_names():
+def get_model_names() -> List[str]:
     """Get a list of available model names."""
     return list(_MODEL_CONFIGS.keys())
 
-def get_model_dict(model_name):
+def get_model_dict(model_name: str) -> dict:
     """Get a model's key/value properties - modify at your own risk."""
     return _MODEL_CONFIGS[model_name]
 
-def get_model_layers(model_name):
+def get_model_layers(model_name: str) -> int:
     """Get a model's layer count."""
     return _MODEL_CONFIGS[model_name]['layers']
 
-def get_model_default_weights_file(model_name):
+def get_model_default_weights_file(model_name: str) -> str:
     """Get a model's default weights file name."""
     return _MODEL_CONFIGS[model_name]['weights_file']
 
-def module_shard_factory(model_name, model_file, layer_start, layer_end, stage):
+def module_shard_factory(model_name: str, model_file: str, layer_start: int, layer_end: int,
+                         stage: int) -> ModuleShard:
     """Get a shard instance on the globally-configured `devices.DEVICE`."""
     # This works b/c all shard implementations have the same constructor interface
     is_first = layer_start == 1
@@ -66,7 +69,9 @@ def _dist_rpc_pipeline_stage_factory(*args, **kwargs) -> rpc.DistRpcPipelineStag
     stage.module_to(device=devices.DEVICE)
     return stage
 
-def dist_rpc_pipeline_factory(model_name, model_file, stage_ranks, stage_layers, results_cb):
+def dist_rpc_pipeline_factory(model_name: str, model_file: str, stage_ranks: List[int],
+                              stage_layers: List[Tuple[int, int]],
+                              results_cb: Callable[[Any], None]) -> rpc.DistRpcPipeline:
     """Get an RPC pipeline instance."""
     # This works b/c all shard implementations have the same constructor interface
     module = _MODEL_CONFIGS[model_name]['shard_module']
