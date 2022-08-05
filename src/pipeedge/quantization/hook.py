709eb28d368079a2fada32eaca5dfaf5db0f3562
Haonan Wang <haonanwa@usc.edu>
2022-08-05 00:22:49 -0700

    per image quant for batched input tensor

--- src/pipeedge/quantization/hook.py
+++ src/pipeedge/quantization/hook.py
@@ -1,7 +1,24 @@
 """Quant functions used to encode/decode tensors in hooks"""
 from typing import Tuple, Union
-from torch import Tensor
+from torch import Tensor, stack
 from .basic_op import tensor_encode, tensor_decode
+
+
+def per_batch_encode(batched_tensor, quant_bit):
+    """do quantization on each image in the micro-batched tensor with size [b,c,h,w]"""
+    data_tuple, shape_tuple, scale_tuple, shift_tuple = [],[],[],[]
+    for sub_tensor in batched_tensor:
+        data_tensor, shape, scale_factor, shift = tensor_encode(sub_tensor, quant_bit)
+        data_tuple.append(data_tensor)
+        shape_tuple.append(shape)
+        scale_tuple.append(scale_factor)
+        shift_tuple.append(shift)
+    batched_data = stack(data_tuple,0)
+    batched_shape = stack(shape_tuple,0)
+    batched_scale = stack(scale_tuple,0)
+    batched_shift = stack(shift_tuple,0)
+    return [batched_data, batched_shape, batched_scale, batched_shift]
+
 
 def forward_hook_quant_encode(module, _input_arg, output: Union[Tensor, Tuple[Tensor, ...]]):
     """encode tensor in the forward hook (after each module)"""
@@ -12,7 +29,8 @@ def forward_hook_quant_encode(module, _input_arg, output: Union[Tensor, Tuple[Te
     comm_tuple = []
     for tensor in output:
         assert isinstance(tensor, Tensor)
-        comm_tuple += tensor_encode(tensor, quant_bits[1])
+        # comm_tuple += tensor_encode(tensor, quant_bits[1])
+        comm_tuple += per_batch_encode(tensor, quant_bits[1])
     return tuple(comm_tuple)
 
 
@@ -23,18 +41,17 @@ def forward_pre_hook_quant_decode(module, input_arg: Tuple[Tuple[Tensor, ...]]):
     # input_tensor: len=4x for x tensors encoded as: comm_tensor, input_shape, scale_factor, shift
     input_tensor = input_arg[0]
     assert isinstance(input_tensor, tuple)
-    assert len(input_tensor) % 4 == 0
+    assert len(input_tensor)%4 == 0
     quant_bits = module.quant_bits.tolist()
     forward_tensor = []
     for i in range(len(input_tensor) // 4):
-        # comm_tensor: 1-dimensional tensor
-        # input_shape: to restore the original shape of the tensor needed for forward
-        # scale_factor: the max value of the original uncompressed tensor
-        # shift: the min value of the original uncompressed tensor
-        comm_tensor, input_shape, scale_factor, shift =\
+        batched_tensor_list = []
+        data_tensors, input_shapes, scale_factors, shift_factors =\
             input_tensor[i*4], input_tensor[i*4+1], input_tensor[i*4+2], input_tensor[i*4+3]
-        forward_tensor.append(
-            tensor_decode(comm_tensor, input_shape, scale_factor, shift, quant_bits[0]))
+        for idx, data_tensor in enumerate(data_tensors):
+            batched_tensor_list.append(tensor_decode(data_tensor, input_shapes[idx], scale_factors[idx], shift_factors[idx], quant_bits[0]))
+        batched_tensor = stack(batched_tensor_list, 0)
+        forward_tensor.append(batched_tensor)
     # Return value(s) should be wrapped in an outer tuple, like input_arg
     # The tuple will be unpacked when forward() is invoked, which must yield a single parameter
     if len(forward_tensor) == 1:
