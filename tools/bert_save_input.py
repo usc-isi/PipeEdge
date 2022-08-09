d010f189b8711fa610fae3bc2a4e2a68f6ded62b
Connor Imes <cimes@isi.edu>
2022-08-09 15:14:07 -0400

    tools: include labels with GLUE CoLA dataset file

--- tools/bert_save_input.py
+++ tools/bert_save_input.py
@@ -7,10 +7,12 @@ ds_train = datasets.load_dataset('glue', name='cola', split='train')
 # from the first ~1024 sentences in the dataset.
 # For simplicity, we'll just take the first 512 (without duplicates).
 bert_input = ds_train[:512]['sentence']
+bert_label = ds_train[:512]['label']
 
 # The following custom string was at index=0 in the original evaluation input file.
 # The intent seems to be to force the tokenizer to produce input_ids with width=512.
 # While there are better ways to achieve that, this is what was done.
 bert_input[0] = 'hello ' * 512
+bert_label[0] = 0
 
-np.savez('bert_input.npz', input=bert_input)
+np.savez('bert_input.npz', input=bert_input, label=bert_label)
