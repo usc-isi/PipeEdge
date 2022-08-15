b66252c431abfb915f0e18d9b65576503e293758
Connor Imes <cimes@isi.edu>
2022-08-15 09:22:02 -0400

    runtime: fix encode hook function to only clamp if quant_bit > 0
    Without this check, a KeyError is raised b/c there's no optimal clamp
    parameter.

--- runtime.py
+++ runtime.py
@@ -92,9 +92,10 @@ def forward_hook_quant_encode(module, _input_arg, output: Union[torch.Tensor, Tu
     comm_tuple = []
     for tensor in output:
         assert isinstance(tensor, torch.Tensor)
-        clamp = clamp_banner2019_laplace if tensor.min()<0.2 else clamp_banner2019_gelu
-        clamped_tensor = clamp(tensor, quant_bit)
-        stacked_tensor = tensor_encode_outerdim(clamped_tensor, quant_bit)
+        if quant_bit > 0:
+            clamp = clamp_banner2019_laplace if tensor.min() < 0.2 else clamp_banner2019_gelu
+            tensor = clamp(tensor, quant_bit)
+        stacked_tensor = tensor_encode_outerdim(tensor, quant_bit)
         comm_tuple += stacked_tensor
     return tuple(comm_tuple)
 
