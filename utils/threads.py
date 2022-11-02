77fff05b45caf78d2c7901bf65f385f2ad5a4eae
Connor Imes <cimes@isi.edu>
2022-11-02 15:55:59 -0400

    utils: add threads module with RWLock class

---
+++ utils/threads.py
@@ -0,0 +1,57 @@
+"""Thread utilities."""
+from contextlib import contextmanager
+import threading
+
+class RWLock:
+    """A multiple-reader, non-reentrant single-writer lock."""
+
+    def __init__(self):
+        self._readers = 0
+        # Use a simple Lock, not Condition's default RLock
+        self._cond = threading.Condition(threading.Lock())
+
+    def acquire_read(self):
+        """Acquire a read lock."""
+        with self._cond:
+            self._readers += 1
+
+    def release_read(self):
+        """Release a read lock."""
+        with self._cond:
+            self._readers -= 1
+            self._cond.notifyAll()
+
+    def acquire_write(self):
+        """Acquire a write lock."""
+        self._cond.acquire()
+        while self._readers > 0:
+            self._cond.wait()
+
+    def release_write(self):
+        """Release a write lock."""
+        self._cond.release()
+
+    @contextmanager
+    def lock_read(self):
+        """A context manager factory function to acquire a read lock."""
+        try:
+            self.acquire_read()
+            yield
+        finally:
+            self.release_read()
+
+    @contextmanager
+    def lock_write(self):
+        """A context manager factory function to acquire a write lock."""
+        try:
+            self.acquire_write()
+            yield
+        finally:
+            self.release_write()
+
+    # Class instance context acquire/release conservatively uses writer lock
+
+    __enter__ = acquire_write
+
+    def __exit__(self, _t, _v, _tb):
+        self.release_write()
