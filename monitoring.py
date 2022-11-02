33f2d876028cd5b48a245f1ee0476d6df7bf082f
Connor Imes <cimes@isi.edu>
2022-11-02 15:58:27 -0400

    monitoring: synchronize with multiple-reader, single-writer lock

--- monitoring.py
+++ monitoring.py
@@ -13,6 +13,7 @@ import os
 import threading
 from typing import Union
 from pipeedge.monitoring import MonitorContext, MonitorIterationContext
+from utils.threads import RWLock
 
 # Environment variables to override parameters
 ENV_WINDOW_SIZE: str = "WINDOW_SIZE"
@@ -28,12 +29,13 @@ logger = logging.getLogger(__name__)
 
 # this really does need to be a global variable (pylint incorrectly assumes it's a constant)
 _monitor_ctx = None # pylint: disable=invalid-name
+_monitor_ctx_lock = RWLock()
 
 # Per-thread iteration contexts
 # key: thread ident, value: dict (key: key, value: MonitorIterationContext)
 _thr_ctx = {}
 
-# Locks are only for reporting iterations, there's no thread safety for managing lifecycle or keys
+# Locks are only for reporting iterations
 _locks = {}
 
 # User-friendly field names
@@ -105,57 +107,61 @@ def init(key: str, work_type: str='items', acc_type: str='acc') -> None:
     window_size = get_window_size()
     log_name = key + '.csv'
     log_mode = os.getenv(ENV_CSV_FILE_MODE, _CSV_FILE_MODE)
-    try:
-        _monitor_ctx = MonitorContext(key=key, window_size=window_size, log_name=log_name,
-                                      log_mode=log_mode)
-        logger.info("Monitoring energy source: %s", _monitor_ctx.energy_source)
-    except FileNotFoundError:
-        _monitor_ctx = MonitorContext(key=key, window_size=window_size, log_name=log_name,
-                                      log_mode=log_mode, energy_lib=None)
-        logger.warning("Couldn't find energymon-default library, disabling energy metrics...")
-    try:
-        _monitor_ctx.open()
-    except OSError as e:
-        # Usually happens if energymon can't be initialized, e.g., b/c the power/energy sensors
-        # aren't available or we don't have permission to access them (which often requires root).
-        logger.error("Error code: %d: %s", e.errno, e.strerror)
-        logger.warning("Couldn't init monitor context, trying without energy metrics...")
-        _monitor_ctx = MonitorContext(key=key, window_size=window_size, log_name=log_name,
-                                      log_mode=log_mode, energy_lib=None)
-        _monitor_ctx.open()
-    _locks[key] = threading.Lock()
-    _work_types[key] = work_type
-    _acc_types[key] = acc_type
+    with _monitor_ctx_lock.lock_write():
+        try:
+            _monitor_ctx = MonitorContext(key=key, window_size=window_size, log_name=log_name,
+                                          log_mode=log_mode)
+            logger.info("Monitoring energy source: %s", _monitor_ctx.energy_source)
+        except FileNotFoundError:
+            _monitor_ctx = MonitorContext(key=key, window_size=window_size, log_name=log_name,
+                                          log_mode=log_mode, energy_lib=None)
+            logger.warning("Couldn't find energymon-default library, disabling energy metrics...")
+        try:
+            _monitor_ctx.open()
+        except OSError as e:
+            # Usually happens if energymon can't be initialized, e.g., b/c the power/energy sensors
+            # aren't available or we don't have access permission (which often requires root).
+            logger.error("Error code: %d: %s", e.errno, e.strerror)
+            logger.warning("Couldn't init monitor context, trying without energy metrics...")
+            _monitor_ctx = MonitorContext(key=key, window_size=window_size, log_name=log_name,
+                                          log_mode=log_mode, energy_lib=None)
+            _monitor_ctx.open()
+        _locks[key] = threading.Lock()
+        _work_types[key] = work_type
+        _acc_types[key] = acc_type
 
 def finish() -> None:
     """Destroy monitoring context."""
     global _monitor_ctx # pylint: disable=global-statement,invalid-name
-    if _monitor_ctx is None:
-        return
-    if _PRINT_FIELDS_GLOBAL:
-        for key in _monitor_ctx.keys():
-            _log_global(key)
-    _monitor_ctx.close()
-    _monitor_ctx = None
-    _thr_ctx.clear()
-    _locks.clear()
-    _work_types.clear()
-    _acc_types.clear()
+    with _monitor_ctx_lock.lock_write():
+        if _monitor_ctx is None:
+            return
+        if _PRINT_FIELDS_GLOBAL:
+            for key in _monitor_ctx.keys():
+                _log_global(key)
+        _monitor_ctx.close()
+        _monitor_ctx = None
+        _thr_ctx.clear()
+        _locks.clear()
+        _work_types.clear()
+        _acc_types.clear()
 
 def add_key(key: str, work_type: str='items', acc_type: str='acc') -> None:
     """Add a new key."""
-    if _monitor_ctx is None:
-        return
-    _monitor_ctx.add_heartbeat(key=key, log_name=key+'.csv')
-    _locks[key] = threading.Lock()
-    _work_types[key] = work_type
-    _acc_types[key] = acc_type
+    with _monitor_ctx_lock.lock_write():
+        if _monitor_ctx is None:
+            return
+        _monitor_ctx.add_heartbeat(key=key, log_name=key+'.csv')
+        _locks[key] = threading.Lock()
+        _work_types[key] = work_type
+        _acc_types[key] = acc_type
 
 @contextmanager
 def get_locked_context(key: str) -> None:
     """Yields the `MonitorContext` with a lock on `key` (use to synchronize retrieving metrics)."""
-    with _locks[key]:
-        yield _monitor_ctx
+    with _monitor_ctx_lock.lock_read():
+        with _locks[key]:
+            yield _monitor_ctx
 
 def _iter_ctx_push(key):
     ident = threading.get_ident()
@@ -180,30 +186,32 @@ def _iter_ctx_pop(key):
 
 def iteration_start(key: str) -> None:
     """Start an iteration."""
-    if _monitor_ctx is None:
-        return
-    with _locks[key]:
-        _monitor_ctx.iteration_start(iter_ctx=_iter_ctx_push(key))
+    with _monitor_ctx_lock.lock_read():
+        if _monitor_ctx is None:
+            return
+        with _locks[key]:
+            _monitor_ctx.iteration_start(iter_ctx=_iter_ctx_push(key))
 
 def iteration(key: str, work: int=1, accuracy: Union[int, float]=0, safe: bool=True) -> None:
     """Complete an iteration."""
-    if _monitor_ctx is None:
-        return
-    with _locks[key]:
-        try:
-            iter_ctx = _iter_ctx_pop(key)
-        except KeyError:
-            # Should only happen if `iteration_start()` isn't used.
-            # The underlying monitoring API allows this, but the thread safety we provide is lost.
-            if safe:
-                raise KeyError(f"No thread iteration context for key: {key}") from None
-            iter_ctx = None
-        _monitor_ctx.iteration(key=key, work=work, accuracy=accuracy, iter_ctx=iter_ctx)
-        # tag=0 only if this call was an initial "start", in which case it's not a real iteration
-        tag = _monitor_ctx.get_tag(key=key)
-        if tag > 0:
-            if _PRINT_FIELDS_INSTANT:
-                _log_instant(key)
-            if _PRINT_FIELDS_WINDOW:
-                if tag > 0 and (tag + 1) % _WINDOW_SIZE == 0:
-                    _log_window(key)
+    with _monitor_ctx_lock.lock_read():
+        if _monitor_ctx is None:
+            return
+        with _locks[key]:
+            try:
+                iter_ctx = _iter_ctx_pop(key)
+            except KeyError:
+                # Should only happen if `iteration_start()` isn't used.
+                # The MonitorContext API allows this, but the thread safety we provide is lost.
+                if safe:
+                    raise KeyError(f"No thread iteration context for key: {key}") from None
+                iter_ctx = None
+            _monitor_ctx.iteration(key=key, work=work, accuracy=accuracy, iter_ctx=iter_ctx)
+            # tag=0 if this call was an initial "start", in which case it's not a real iteration
+            tag = _monitor_ctx.get_tag(key=key)
+            if tag > 0:
+                if _PRINT_FIELDS_INSTANT:
+                    _log_instant(key)
+                if _PRINT_FIELDS_WINDOW:
+                    if tag > 0 and (tag + 1) % _WINDOW_SIZE == 0:
+                        _log_window(key)
